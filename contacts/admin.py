d3bdb0e270e7bb496bda377f7d67e610eb52def5
[sawar007] <[arnav0072011@gmail.com]>
2019-05-09 02:13:56 +0530

    First

---
+++ contacts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','name','listing','email','contact_date' )
+    list_display_links = ('id','name')
+    
+
+
+admin.site.register(Contact, ContactAdmin)
